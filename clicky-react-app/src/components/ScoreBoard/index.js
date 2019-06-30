44cd6408ef48c5825f8fa84199919ed911b665ae
Mike Hageali <45021989+zmanphx@users.noreply.github.com>
2019-06-30 13:28:58 -0700

    adding styling

--- clicky-react-app/src/components/ScoreBoard/index.js
+++ clicky-react-app/src/components/ScoreBoard/index.js
@@ -1,13 +1,16 @@
 import React from "react";
 import "./style.css";
 
-function ScoreBoard(props) {
+function  ScoreBoard (props) {
+ 
+ console.log("props",props);
   return (
     <div className="Score">
-      {" "}
+      
       <h2>
-        Current Score: <span id="CurrentScore">score </span> Top Score: 
-        <span id="topscore"> topscore</span>
+        
+        Current Score: <span id="CurrentScore">{props.children[0]} </span> Top Score: 
+        <span id="topscore"> {props.children[2]}</span>
       </h2>{" "}
     </div>
   );
