55b3d444b9add4ac34ad5df8c11c777a9681c8ba
Mike Hageali <45021989+zmanphx@users.noreply.github.com>
2019-06-29 22:30:23 -0700

    adding cards

--- clicky-react-app/src/App.js
+++ clicky-react-app/src/App.js
@@ -1,26 +1,47 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import PlayCard from "./components/PlayCards";
+import Wrapper from "./components/Wrapper";
+import Title from "./components/Title";
+import cards from "./cards.json";
+import ScoreBoard from "./components/ScoreBoard";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  // Setting this.state.friends to the friends json array
+  state = {
+    cards
+  };
+
+  RecordClick = id => {
+    // Filter this.state.friends for friends with an id not equal to the id being removed
+    const cards = this.state.cards.filter(card => card.id !== id);
+    // Set this.state.friends equal to the new friends array
+    this.setState({ cards });
+  };
+
+  // Map over this.state.friends and render a FriendCard component for each friend object
+  render() {
+    return (
+     
+     
+     <Wrapper>
+       
+       
+        <Title>Playing Cards</Title>
+      <ScoreBoard></ScoreBoard>
+        
+        {this.state.cards.map(card => (
+          <PlayCard
+            cards={this.RecordClick}
+            id={card.id}
+            key={card.id}
+            name={card.name}
+            image={card.image}
+           
+          />
+        ))}
+      </Wrapper>
+    );
+  }
 }
 
 export default App;
